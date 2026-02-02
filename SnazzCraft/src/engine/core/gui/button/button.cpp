479f1e91ef9bcef112f191c5dec00253bdf73a07
Mr-Snazz <mr_snazz@outlook.com>
2026-02-02 09:51:11 -0800

    Changed default world size

--- SnazzCraft/src/engine/core/gui/button/button.cpp
+++ SnazzCraft/src/engine/core/gui/button/button.cpp
@@ -42,7 +42,7 @@ void SetWorldModeButtonCallback(SnazzCraft::Event* Event)
     SnazzCraft::World* NewWorld = SnazzCraft::CurrentWorld;
 
     if (NewWorld == nullptr) { 
-        NewWorld = SnazzCraft::CreateWorld("default-generated-world", 3, 2338);
+        NewWorld = SnazzCraft::CreateWorld("default-generated-world", 12, 2338);
         NewWorld->OptimizeChunks();
     }
 
