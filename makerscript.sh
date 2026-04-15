cc156588d1d158b686d4e22b8d95cc05873d7f6c
Johnny Bliss <mr_snazz@outlook.com>
2026-04-15 10:09:47 -0700

    Simplify makerscript.sh by removing unused sections
    Removed unnecessary directory creation and commented code for launcher setup.

--- makerscript.sh
+++ makerscript.sh
@@ -2,23 +2,7 @@ ScriptDirectory="$(cd "$(dirname "$0")" && pwd)"
 
 cd "$ScriptDirectory/SnazzCraft"
 
-mkdir -p "build"
-mkdir -p "bin"
 mkdir -p "worlds"
 
 make all
-
-cd "$ScriptDirectory/Launcher"
-
-#if [ ! -d "pyinstaller-env" ]; then
-#    python3 -m venv --system-site-packages pyinstaller-env
-#fi
-
-#source pyinstaller-env/bin/activate
-#pip install --upgrade pip
-#pip install pyinstaller
-
-#pyinstaller --hidden-import=tkinter --onefile --name=launcher src/launcher.py
-
-cd $ScriptDirectory
-./Launcher/dist/launcher
+./bin/executable
